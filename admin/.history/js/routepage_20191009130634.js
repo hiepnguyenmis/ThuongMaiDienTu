fa50f86b448a37069c3a775c31ac19ec0085db3a
Thanh Tâm Huỳnh <tamht298@gmail.com>
2019-10-09 13:51:28 +0700

    fixed button in right

--- admin/.history/js/routepage_20191009130634.js
+++ admin/.history/js/routepage_20191009130634.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ['ui.router']);
+var app = angular.module("myApp", ['ui.router', 'vcRecaptcha']);
 app.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/home');
     $stateProvider
