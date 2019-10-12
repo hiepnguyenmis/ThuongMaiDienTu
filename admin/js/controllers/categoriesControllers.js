d57f19f9b29d62aff2408267e4d2d7ab49f33248
Nguyen Van Hiep <Vanhiep1998@gmail.com>
2019-10-12 20:29:39 +0700

    create Category

--- admin/js/controllers/categoriesControllers.js
+++ admin/js/controllers/categoriesControllers.js
@@ -1,12 +1,24 @@
 (function(module){
     module.controller('categoriesControllers',function($scope,$http){
         $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
+        const baseUrl="http://localhost:8080/api/categories"
         $http({
             method: "GET",
-            url: "http://localhost:8080/api/categories"
+            url: baseUrl
         }).then(function mySuccess(response){
             $scope.categories=response.data;
         });
+        $scope.createCategory=function(){
+            $http({
+                method: 'POST',
+                url: baseUrl,
+                data: { name: 'test' }
+               }
+            ).then(function(){
+                alert("thêm thành công");
+            });
+        };
+        
     });
+
 }(angular.module("myApp")));
