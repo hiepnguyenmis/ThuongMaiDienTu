852a601afb01f7e7c8c359766beb30151dfe9740
Nguyen Van Hiep <Vanhiep1998@gmail.com>
2019-10-13 17:24:50 +0700

    call service create,update,delete category

--- admin/js/controllers/categoriesControllers.js
+++ admin/js/controllers/categoriesControllers.js
@@ -1,24 +1,50 @@
 (function(module){
-    module.controller('categoriesControllers',function($scope,$http){
-        $http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-        const baseUrl="http://localhost:8080/api/categories"
+    module.controller('categoriesControllers',function($scope,$http,$window){
+        //$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+        var baseUrl='http://localhost:8080/api/categories/';
+        
+        
         $http({
             method: "GET",
-            url: baseUrl
+            url:baseUrl
         }).then(function mySuccess(response){
             $scope.categories=response.data;
         });
-        $scope.createCategory=function(){
-            $http({
-                method: 'POST',
-                url: baseUrl,
-                data: { name: 'test' }
-               }
-            ).then(function(){
-                alert("thêm thành công");
+        $scope.createCategory = function(){
+            let data = {name: $scope.name};
+            
+            $http.post(baseUrl, data).then((response)=>{
+                $window.location.reload();
+            }, (err)=>{
+                alert('Thêm thất bại');
             });
-        };
+        }
+        $scope.updateCategory= function(id,dataName){
+            let data={
+                name:dataName};
+                $http.put(baseUrl+id, data).then((response)=>{
+                    $window.location.reload();
+                }, (err)=>{
+                    alert('sửa thất bại');
+                });
+        }
+        $scope.deleteCategory=function(category){
+            
+            console.log(category.id);
+            
+            // $http.delete(baseUrl+category.id,data).then((response)=>{
+            //     $window.location.reload();
+            // }, (err)=>{
+            //     alert('sửa thất bại');
+            // });
+           $http.delete(baseUrl+category.id).then((response)=>{
+               console.log('xoá ok');
+               
+           }, (err)=>{
+               console.log(err);
+               
+           })
+        }
         
     });
-
 }(angular.module("myApp")));
