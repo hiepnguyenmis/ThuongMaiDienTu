ce75b1d132958df998381937b0f079b895c772e8
Nguyen Van Hiep <Vanhiep1998@gmail.com>
2019-11-05 22:10:44 +0700

    edit single product page

--- store-ecommerce/js/controllers/single_product_controllers.js
+++ store-ecommerce/js/controllers/single_product_controllers.js
@@ -1,13 +1,20 @@
 // Code goes here
 (function(module){
 	module.controller('single_product_controllers', function($scope) {
-		$scope.change_image_slider= function(image_slider){
-			$scope.image=[
-				{id:"1",front_image:"../images/m1.jpg"},
-				{id:"2",back_image:"../images/si1.jpg"},
-				{id:"3",left_image:"../images/si1.jpg"},
-				{id:"4",right_image:"../images/m1.jpg"}
-			];
+		$scope.image=[
+			{id:"1",img:"/images/si1.jpg"},
+			{id:"2",img:"/images/si1.jpg"},
+			{id:"3",img:"/images/si1.jpg"},
+			{id:"4",img:"/images/si1.jpg"}
+		];
+		$scope.url_image=$scope.image[0].img;
+
+		$scope.change_image_slider= function(id_image,url){
+			
+			$scope.url_image=url;
+			if(id_image=$scope.image.id){
+				$scope.url_image=$scope.image.img;
+			}
 		}
 	});	  
 }(angular.module('myApp')));
