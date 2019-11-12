b277d49211197892b2946e57ab31d05336b5b7f2
Nguyen Van Hiep <Vanhiep1998@gmail.com>
2019-11-12 20:57:45 +0700

    productsCtrl, interface products

--- admin/js/controllers/productsControllers.js
+++ admin/js/controllers/productsControllers.js
@@ -1,6 +1,6 @@
 (function(module){
     module.controller('productsControllers',function($scope,$http){
-        var baseUrl='http://localhost:8080/api/products';
+        var baseUrl='http://localhost:8080/api/products/';
         this.$onInit = function(){
             $scope.getdataProducts();
         }
@@ -10,6 +10,41 @@
                 url:baseUrl
             }).then(function mySuccess(response){
                 $scope.products=response.data;
+            });
+        }
+        $scope.getdataCategory=function(){
+            $http({
+                method: "GET",
+                url:'http://localhost:8080/api/categories/'
+            }).then(function mySuccess(response){
+                $scope.categories=response.data;
+            });
+        }
+        // $scope.createProduct= function(idCategory){
+            
+        //     var data={
+        //         productCode:$scope.productCode,
+        //         name:$scope.Name,
+        //         price: $scope.Price,
+        //         thumbnail: $scope.Thumbnail,
+        //         stock: $scope.Stock,
+        //         cpu:$scope.cpu,
+        //         ram: $scope.ram,
+        //         hardDisk: $scope.hardDisk,
+        //         screen: $scope.screen,
+        //         category: idCategory,
+
+
+
+        //     };
+
+        // }
+        $scope.deleteProduct=function(id){
+            $http.delete(baseUrl+id).then((response)=>{
+                $scope.getdataProducts();
+                alert('Xóa thành công!');
+            }, (err)=>{
+                alert('Xóa thất bại');
             });
         }
     });
