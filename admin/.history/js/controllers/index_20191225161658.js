966d32e560a565bdedd8d91c1ea163281a435b9c
Huynh Thanh Tam <46307504+tamht298@users.noreply.github.com>
2019-12-26 19:43:19 +0700

    add db

---
+++ admin/.history/js/controllers/index_20191225161658.js
@@ -0,0 +1,9 @@
+(function(module){
+    module.controller('indexControllers',function($localStorage,$scope){
+        
+
+        $scope.username=$localStorage.currentUser.email;
+        console.log($localStorage.currentUser.email);
+        
+    });
+}(angular.module("myApp", "['naif.base64']")));
---
+++ admin/.history/js/controllers/productsControllers_20191226152757.js
@@ -0,0 +1,124 @@
+(function(module){
+    module.controller('productsControllers',function($scope,$http){
+        //$http.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+        var baseUrl='http://localhost:8080/api/';
+        $scope.selectedCategory=-1;
+        $scope.updatedCategory=-1;
+        $scope.category={};
+        $scope.thumbnail = [];
+        $scope.images=[];
+        $scope.image1 = [];
+        $scope.image2 = [];
+        this.$onInit = function(){
+            $scope.loading= true;
+           
+            $scope.getdataProducts();
+            $scope.getdataCategory();
+        }
+        $scope.getdataProducts=function(){
+            $http({
+                method: "GET",
+                url:baseUrl+'products'
+            }).then(function mySuccess(response){
+                console.info('Fetching')
+                $scope.products=response.data;
+                $scope.loading = false;
+            });
+        }
+        $scope.getdataCategory=function(){
+            $http({
+                method: "GET",
+                url:baseUrl+'categories'
+            }).then(function mySuccess(response){
+                $scope.categories=response.data;
+            });
+        }
+
+        $scope.getCategoryById= function(id){
+            $http.get(baseUrl+'categories/'+id).then(function success(res){
+                $scope.category = res.data;
+              
+                
+            })
+        }
+
+        $scope.getSelected = function(cateId){
+            $scope.updatedCategory=cateId;
+            $scope.getCategoryById(cateId)
+            
+        }
+
+        $scope.createProduct= function(){
+            
+            // $scope.images=[
+            //     {path:$scope.image_back},
+            //     {path:$scope.image_front},
+            //     {path:$scope.image_left},
+            //     {path:$scope.image_right}
+            // ]
+
+            console.log($scope.image1.);
+            
+            var data={
+                productCode:$scope.productCode,
+                name:$scope.Name,
+                price: $scope.Price,
+                thumbnail: $scope.thumbnail.base64,
+                stock: $scope.Stock,
+                cpu:$scope.cpu,
+                ram: $scope.ram,
+                hardDisk: $scope.hardDisk,
+                screen: $scope.screen,
+                category: $scope.category,
+                images:null
+            };
+
+            console.log(data);
+            
+            $http.post(baseUrl+'products', data).then((response)=>{
+                
+                $scope.getdataProducts();
+                $scope.Name='';
+                //$scope.Thumbnail=""
+                $scope.Stock='';
+                $scope.cpu='';
+                $scope.ram='';
+                $scope.hardDisk='';
+                $scope.screen='';
+               
+            }, (err)=>{
+                alert('Thêm thất bại');
+            });
+        }
+
+        $scope.updateProduct = function(product){
+            product.category=$scope.category;
+            console.log(product);
+            console.log(product.category.id);
+            
+            
+            $http.put(baseUrl+'products/'+product.id, product).then((res)=>{
+                $scope.getdataProducts();
+                document.getElementById('closeUpdateModal'+product.id).click();
+                $scope.updatedCategory=-1;
+                $scope.sucMessage = 'Cập nhật thông tin thành công!';
+            }, (err)=>{
+                console.error(err);
+                $scope.errMessage = 'Cập nhật thông tin thất bại!';
+                alert('Cập nhật thất bại')
+            })
+        }
+
+        $scope.deleteProduct=function(id){
+            $http.delete(baseUrl+'products/'+id).then((res)=>{
+                $scope.getdataProducts();
+            }, (err)=>{
+                alert('xóa thất bại');
+            })
+        }
+
+    });
+    
+
+
+    }(angular.module('myApp')));
