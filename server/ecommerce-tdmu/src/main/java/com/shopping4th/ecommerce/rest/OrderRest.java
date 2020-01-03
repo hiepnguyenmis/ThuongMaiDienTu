beb1210f0470f997497d9363354eda7b6db09048
Huynh Thanh Tam <46307504+tamht298@users.noreply.github.com>
2020-01-03 10:00:39 +0700

    api for add order

--- server/ecommerce-tdmu/src/main/java/com/shopping4th/ecommerce/rest/OrderRest.java
+++ server/ecommerce-tdmu/src/main/java/com/shopping4th/ecommerce/rest/OrderRest.java
@@ -1,12 +1,19 @@
 package com.shopping4th.ecommerce.rest;
 
+import java.util.Date;
 import java.util.List;
+
+import javax.persistence.criteria.Order;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,12 +35,30 @@ public class OrderRest {
 	} 
 	
 	@GetMapping(value="orders/{id}")
-	public Orders getProduct(@PathVariable Long id) {
+	public Orders getOrder(@PathVariable Long id) {
 		boolean isOrder = this.orderService.existsById(id);
 		if(!isOrder) {                                       
 			throw new RuntimeException("Order "+ id + " is not found");
 		}
 		
 		return orderService.findById(id);
+	}
+	
+	@DeleteMapping(value="orders/{id}")
+	public void deleteOrder(@PathVariable Long id) {
+		boolean isOrder = this.orderService.existsById(id);
+		if(!isOrder) {
+			throw new RuntimeException("Product "+ id + " is not found");
+		}
+		
+		orderService.deletedById(id);
+		
+	}
+	
+	@PostMapping(value="/orders")
+	public Orders createOrder(@Valid @RequestBody Orders order) {
+		order.setCreatedAt(new Date());
+		orderService.save(order);
+		return orderService.findById(order.getId());
 	}
 }
